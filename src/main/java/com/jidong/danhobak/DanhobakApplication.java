42e4aae0ce2357b0674ec099b58299db4085cb29
jiwonKown <jiwonk419@11st.co.kr>
2020-01-19 18:28:48 +0900

    first commit

---
+++ src/main/java/com/jidong/danhobak/DanhobakApplication.java
@@ -0,0 +1,13 @@
+package com.jidong.danhobak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanhobakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanhobakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jidong/danhobak/DanhobakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jidong.danhobak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanhobakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
